74c1a498bb6566bb6e2a33afc39ff938efe2180f
Ignace Dewulf <ignace.dewulf2@gmail.com>
2026-03-31 13:39:59 +0200

    added the curling stone controller script

---
+++ MineCurling/Assets/Scripts/CurlingStoneController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class CurlingStoneController : MonoBehaviour
+{
+    //private PhysicsMaterial _
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
