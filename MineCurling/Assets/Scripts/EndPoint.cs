edda1fdaabf2779ed6276812bbf827d1bf8889ec
Mathis <mathispfaff07@gmail.com>
2026-03-31 19:31:47 +0200

    small little change to the camera distance

---
+++ MineCurling/Assets/Scripts/EndPoint.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class EndPoint : MonoBehaviour
+{
+    [SerializeField] private SphereCollider _threePointColliders;
+    [SerializeField] private SphereCollider _twoPointColliders;
+    [SerializeField] private SphereCollider _onePointColliders;
+
+
+}
