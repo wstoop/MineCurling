486b2fe648eb9e4dddbf645d6aa0d5fcbcc53c6a
Ignace Dewulf <ignace.dewulf2@gmail.com>
2026-04-01 21:06:58 +0200

    added braces

--- MineCurling/Assets/Scripts/CurlingStoneController.cs
+++ MineCurling/Assets/Scripts/CurlingStoneController.cs
@@ -82,7 +82,7 @@ public class CurlingStoneController : MonoBehaviour
 
     private void Update()
     {
-        if (_rigidbody.linearVelocity.sqrMagnitude < _minimumVelocityThreshold && !_isStoppable)
+        if ((_rigidbody.linearVelocity.sqrMagnitude < _minimumVelocityThreshold) && !_isStoppable)
         {
             _rigidbody.linearVelocity = _rigidbody.linearVelocity.normalized * _minimumVelocityThreshold;
         }
