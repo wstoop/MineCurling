b2d8d2140892f37d25de98f1f013d0514bb3dcd7
wstoop <warre.stoop@gmail.com>
2026-04-01 15:04:02 +0200

    Added multiplayeer suppport (sort of, still working on it)

---
+++ MineCurling/Assets/Scripts/PlayerSelectedSpriteManager.cs
@@ -0,0 +1,51 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Splines;
+using UnityEngine.UI;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Composites;
+
+public class PlayerSelectedSpriteManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private List<GameObject> _sprites;
+    [SerializeField]
+    private List<GameObject> _joinedSprites;
+    [SerializeField]
+    private GameObject _startgameImage;
+    [SerializeField]
+    private InputActionReference buttonApressed;
+
+    private List<Image> _spritesImages = new List<Image>();
+    private List<Image> _joinedSpritesImages = new List<Image>();
+    private int _playerCount = 0;
+    private void Awake()
+    {
+        foreach (var sprite in _sprites)
+        {
+            _spritesImages.Add(sprite.GetComponent<Image>());
+        }
+        foreach (var joinedSprite in _joinedSprites)
+        {
+            _joinedSpritesImages.Add(joinedSprite.GetComponent<Image>());
+        }
+         
+    }
+
+    public void AddPlayer()
+    {
+        if (_playerCount >= 4) return;
+        if (_playerCount == 0)
+        { 
+            _startgameImage.SetActive(true);
+            buttonApressed.action.performed += ctx => gameObject.SetActive(false);
+        }
+        _playerCount++;
+        _spritesImages[_playerCount - 1].enabled = false;
+        _joinedSpritesImages[_playerCount - 1].enabled = true;
+    }
+
+
+}
--- MineCurling/Assets/Scripts/SceneController.cs
+++ MineCurling/Assets/Scripts/SceneController.cs
@@ -1,78 +1,36 @@
-using NUnit.Framework;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    [SerializeField]
-    private PlayerInputManager _inputManager;
-    [SerializeField]
-    private List<GameObject> _startScreenElements;
-    [SerializeField]
-    private List<GameObject> _registerScreenElements;
-    [SerializeField]
-    private List<GameObject> _gameScreenElements;
-    [SerializeField]
-    private List<GameObject> _endScreenElements;
-
-    public enum ScreenType
+    // Loads a scene by name
+    public void LoadScene(string sceneName)
     {
-        StartScreen,
-        RegisterScreen,
-        GameScreen,
-        EndScreen
+        SceneManager.LoadScene(sceneName);
     }
 
-    private void Awake()
+    // Loads a scene by index
+    public void LoadScene(int sceneIndex)
     {
-        _inputManager.enabled = false;
-        SetActiveElements(_registerScreenElements, false);
-        SetActiveElements(_gameScreenElements, false);
-        SetActiveElements(_endScreenElements, false);
-
-        SetActiveElements(_startScreenElements, true);
-        Time.timeScale = 0;
+        SceneManager.LoadScene(sceneIndex);
     }
 
-    private void SetActiveElements(List<GameObject> elements, bool state)
+    // Loads a scene asynchronously (for smoother transitions)
+    public void LoadSceneAsync(string sceneName)
     {
-        foreach (var element in elements)
-        {
-            element.SetActive(state);
-        }
-    }
-    public void SetCurrentScreen(string screenName)
-    {
-        ScreenType screen = (ScreenType)System.Enum.Parse(typeof(ScreenType), screenName);
-
-        //_inputManager.enabled = false;
-        SetActiveElements(_startScreenElements, false);
-        SetActiveElements(_registerScreenElements, false);
-        SetActiveElements(_gameScreenElements, false);
-        SetActiveElements(_endScreenElements, false);
-        switch (screen)
-        {
-            case ScreenType.StartScreen:
-                SetActiveElements(_startScreenElements, true);
-                break;
-            case ScreenType.RegisterScreen:
-                SetActiveElements(_registerScreenElements, true);
-                _inputManager.enabled = true;
-                break;
-            case ScreenType.GameScreen:
-                SetActiveElements(_gameScreenElements, true);
-                Time.timeScale = 1f;
-                break;
-            case ScreenType.EndScreen:
-                SetActiveElements(_endScreenElements, true);
-                break;
-        }
+        SceneManager.LoadSceneAsync(sceneName);
     }
 
+    // Reloads the current scene
+    public void ReloadScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Quits the game (works in standalone builds)
     public void QuitGame()
     {
+        Debug.Log("Quitting game...");
         Application.Quit();
     }
 }
