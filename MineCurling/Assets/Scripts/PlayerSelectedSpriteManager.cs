18390766c811746a96800ed832a4f3dd53ab4adf
wstoop <warre.stoop@gmail.com>
2026-04-01 16:14:59 +0200

    Added multiplayer? I think?

--- MineCurling/Assets/Scripts/PlayerSelectedSpriteManager.cs
+++ MineCurling/Assets/Scripts/PlayerSelectedSpriteManager.cs
@@ -13,6 +13,7 @@ public class PlayerSelectedSpriteManager : MonoBehaviour
     private List<GameObject> _sprites;
     [SerializeField]
     private List<GameObject> _joinedSprites;
+
     [SerializeField]
     private GameObject _startgameImage;
     [SerializeField]
@@ -21,7 +22,7 @@ public class PlayerSelectedSpriteManager : MonoBehaviour
     private List<Image> _spritesImages = new List<Image>();
     private List<Image> _joinedSpritesImages = new List<Image>();
     private int _playerCount = 0;
-    private void Awake()
+    private void OnValidate()
     {
         foreach (var sprite in _sprites)
         {
@@ -36,16 +37,28 @@ public class PlayerSelectedSpriteManager : MonoBehaviour
 
     public void AddPlayer()
     {
-        if (_playerCount >= 4) return;
-        if (_playerCount == 0)
-        { 
-            _startgameImage.SetActive(true);
-            buttonApressed.action.performed += ctx => gameObject.SetActive(false);
+        if(_spritesImages.Count == 0 || _joinedSpritesImages.Count == 0)
+        {
+            foreach (var sprite in _sprites)
+            {
+                _spritesImages.Add(sprite.GetComponent<Image>());
+            }
+            foreach (var joinedSprite in _joinedSprites)
+            {
+                _joinedSpritesImages.Add(joinedSprite.GetComponent<Image>());
+            }
+        }
+        if (_playerCount < 4 && _playerCount > 0)
+        {
+            if (_playerCount == 1)
+            {
+                _startgameImage.SetActive(true);
+                buttonApressed.action.performed += ctx => gameObject.SetActive(false);
+            }
+            
+            _spritesImages[_playerCount - 1].enabled = false;
+            _joinedSpritesImages[_playerCount - 1].enabled = true;
         }
         _playerCount++;
-        _spritesImages[_playerCount - 1].enabled = false;
-        _joinedSpritesImages[_playerCount - 1].enabled = true;
     }
-
-
 }
